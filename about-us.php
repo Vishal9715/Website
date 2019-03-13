148c541e51ecf6939350976f89174853eca81ca8
Vishal9715 <vishal.9715@gnindia.dronacharya.info>
2019-03-13 10:21:48 +0530

    Add files via upload

---
+++ about-us.php
@@ -0,0 +1,200 @@
+<?php include 'header.php';?>
+        <!--================Header Menu Area =================-->
+        
+        <!--================Home Banner Area =================-->
+        <body>
+        <section class="banner_area">
+            <div class="banner_inner d-flex align-items-center">
+            	<div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
+				<div class="container">
+					<div class="banner_content text-center">
+						<h2>About Us</h2><br>
+						<p style='color:white;font-size:20px'>Creating Vision To Your Dreams</p>
+
+					</div>
+				</div>
+            </div>
+        </section>
+        <!--================End Home Banner Area =================-->
+        
+        <!--================About Area =================-->
+        <!-- <section class="about_area p_120" style='background: #f2f2f5;'>
+        	<div class="container">
+        		<div class="main_title" style='margin-bottom:60px'>
+        			<h2>Constant Classes Ecosystem</h2>
+        		</div>
+			
+        		<div class="about_details">
+        			<p style='font-family: "Open Sans", sans-serif;text-align: justify;font-size: 1.25rem;
+    font-weight: 300;line-height: 26px;
+    color: #333;'>
+					Constant Classes – A Step Towards Success is an initiative of kindness to help children
+achieve success and chase their dreams with hand holding, guidance and mentoring. Weat
+ Constant Classes, help children to visualise their dreams into concepts and drive their concepts full
+ of dreams towards the first step of success by channelising them in a right
+direction.
+We at Constant Classes make learning sessions like fun sessions full of visualising
+ concepts so that A Boring Mathematics Class for a child will become their Favourite
+ ConstantClass. The sessions are made interactive by two-way learning ecosystem.
+					</p>
+        		</div>
+        	</div>
+        </section> -->
+        <!--================End About Area =================-->
+        
+        <!-- ================Team Area ================= -->
+         <!-- <section class="team_area p_120 ">
+        	<div class="container">
+        		<div class="main_title">
+        			<h2>Meet Our Faculty</h2>
+        			<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s exciting to think about setting up your own viewing station.</p>
+        		</div>
+        		<div class="row team_inner">
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="team_item">
+        					<div class="team_img">
+        						<img class="rounded-circle" src="img/team/team-1.jpg" alt="">
+        						<div class="hover">
+        							<a href="#"><i class="fa fa-facebook"></i></a>
+        							<a href="#"><i class="fa fa-twitter"></i></a>
+        							<a href="#"><i class="fa fa-linkedin"></i></a>
+        						</div>
+        					</div>
+        					<div class="team_name">
+        						<h4>Ethel Davis</h4>
+        						<p>Managing Director (Sales)</p>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="team_item">
+        					<div class="team_img">
+        						<img class="rounded-circle" src="img/team/team-2.jpg" alt="">
+        						<div class="hover">
+        							<a href="#"><i class="fa fa-facebook"></i></a>
+        							<a href="#"><i class="fa fa-twitter"></i></a>
+        							<a href="#"><i class="fa fa-linkedin"></i></a>
+        						</div>
+        					</div>
+        					<div class="team_name">
+        						<h4>Ethel Davis</h4>
+        						<p>Managing Director (Sales)</p>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="team_item">
+        					<div class="team_img">
+        						<img class="rounded-circle" src="img/team/team-3.jpg" alt="">
+        						<div class="hover">
+        							<a href="#"><i class="fa fa-facebook"></i></a>
+        							<a href="#"><i class="fa fa-twitter"></i></a>
+        							<a href="#"><i class="fa fa-linkedin"></i></a>
+        						</div>
+        					</div>
+        					<div class="team_name">
+        						<h4>Ethel Davis</h4>
+        						<p>Managing Director (Sales)</p>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="team_item">
+        					<div class="team_img">
+        						<img class="rounded-circle" src="img/team/team-4.jpg" alt="">
+        						<div class="hover">
+        							<a href="#"><i class="fa fa-facebook"></i></a>
+        							<a href="#"><i class="fa fa-twitter"></i></a>
+        							<a href="#"><i class="fa fa-linkedin"></i></a>
+        						</div>
+        					</div>
+        					<div class="team_name">
+        						<h4>Ethel Davis</h4>
+        						<p>Managing Director (Sales)</p>
+        					</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section> -->
+        <!--================End Team Area =================-->
+        
+        <!--================Testimonials Area =================-->
+        <section class="testimonials_area p_120">
+        	<div class="container">
+        		<div class="testi_slider owl-carousel">
+        			<div class="item">
+        				<div class="testi_item" style='border-color: red;border-width: thick;'>
+							<img src="img/mk.png" alt="">
+       						<h1 style='color:black;margin-top:12px;margin-bottom:1.5rem'>Constant Classes Ecosystem</h1>
+							   <p style='font-family: "Open Sans", sans-serif;text-align: justify;font-size: 18px;
+    font-weight: 300;line-height: 26px;
+    color: #333;'>
+				<b style='font-weight:bold'><u style='color:black;'>
+					Constant Classes </u></b>– A Step Towards Success is an initiative of kindness to help children
+achieve success and chase their dreams with hand holding, guidance and mentoring. Weat
+ Constant Classes, help children to visualise their dreams into concepts and drive their concepts full
+ of dreams towards the first step of success by channelising them in a right
+direction.
+We at Constant Classes make learning sessions like fun sessions full of visualising
+ concepts so that A Boring Mathematics Class for a child will become their Favourite
+ ConstantClass. The sessions are made interactive by two-way learning ecosystem. </p>   
+							   <!-- <ul class="list">
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       						</ul>
+       						<p>Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.</p> -->
+        				</div>
+        			</div>
+        			<!-- <div class="item">
+        				<div class="testi_item">
+							<img src="img/testimonials/testi-3.png" alt="">
+       						<h4>Fannie Rowe</h4>
+       						<ul class="list">
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       						</ul>
+       						<p>Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.</p>
+        				</div>
+        			</div>
+        			<div class="item">
+        				<div class="testi_item">
+							<img src="img/testimonials/testi-3.png" alt="">
+       						<h4>Fannie Rowe</h4>
+       						<ul class="list">
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       						</ul>
+       						<p>Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.</p>
+        				</div>
+        			</div> -->
+        		</div>
+        	</div>
+        </section> 
+        <!--================End Testimonials Area =================-->
+        
+        <!-- ================Impress Area =================
+        <section class="impress_area p_120">
+        	<div class="container">
+        		<div class="impress_inner text-center">
+					<h2>Become an instructor</h2>
+					<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s exciting to think about setting up your own viewing station whether that is on the deck</p>
+					<a class="main_btn2" href="#">Apply for the post</a>
+        		</div>
+        	</div>
+        </section> -->
+</body>
+        <!--================End Impress Area =================-->
+        
+        <!--================ start footer Area  =================-->	
+<?php include 'footer.php';?>
+</html>
---
+++ vision.php
@@ -0,0 +1,248 @@
+<?php include 'header.php';?>
+        <!--================Header Menu Area =================-->
+        
+        <!--================Home Banner Area =================-->
+        <body>
+        <section class="banner_area ">
+            <div class="banner_inner d-flex align-items-center">
+            	<div class="overlay bg-parallax success" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
+				<div class="container">
+					<div class="banner_content text-center">
+						<h2>About Us</h2><br>
+						<p style='color:white;font-size:20px'>Creating Vision To Your Dreams</p>
+
+					</div>
+				</div>
+            </div>
+        </section>
+        <!--================End Home Banner Area =================-->
+        
+        <!--================About Area =================-->
+       <!-- <section class="about_area p_120" style='background: #f2f2f5;'>
+        	<div class="container">
+        		<div class="main_title" style='margin-bottom:60px'>
+        			<h2><u style='color:black'>Vision & Mission</u></h2>
+        		</div>
+			
+        		<div class="about_details">
+        			 <p style='font-family: "Open Sans", sans-serif;text-align: justify;font-size: 18px;
+    font-weight: 300;line-height: 26px;
+    color: #333;'>
+				<b style='font-weight:bold'><u style='color:black;'>Dream of Kindness-Free for Differently Abled Kids</u></b>:
+One of the wonderful initiatives of kindness started by our institution, Constant Classes, is
+helping the “Differently Abled” candidates, taking a first step towards success in achieving
+and visualising their dreams at NO COST.We at Constant Classes welcome all differently
+abled children having dreams in eyes and want to learn in the environment of equality and
+pride under the guidance of a friendly mentor cum guide cum friend. </p>
+<p style='font-family: "Open Sans", sans-serif;text-align: justify;font-size: 18px;
+    font-weight: 300;line-height: 26px;
+    color: #333;'>
+<b style='font-weight:bold'><u style='color:black;'>Intuitive Learning Sessions:</u><b>
+The sessions at Constant Classes will be taken by Ms. Deepika Goyal (Former Assistant 
+Professor of Dronacharya College of Engineering and Former Mathematics Faculty for IIT
+JEE in Bansal Classes).We strive towards making children learn and visualise concepts of
+maths by mentoring and guiding them and making them so passionate to remove the fear of
+mathematics and feel mathematics as their fun-friend.</p>
+<p style='font-family: "Open Sans", sans-serif;text-align: justify;font-size: 18px;
+    font-weight: 300;line-height: 26px;
+    color: #333;'>A good learning is done when done with friends and the tutor at Constant Classes is not just
+a mentor or a guide, she is Band of Friendship and let children love mathematics. The only
+way to achieve a success in a great work is to love what you do.Intuitive Sessions at
+Constant Classes drive children towards visualisation of concepts of mathematics with ease
+and achieve great success. </p>
+<p style='font-family: "Open Sans", sans-serif;text-align: justify;font-size: 18px;
+    font-weight: 300;line-height: 26px;
+	color: #333;'>
+	<b style='font-weight:bold'><u style='color:black;'>Cross learning- Doubt Sessions anytime-anywhere:</u><b>
+We at Constant Classes not only help a child to visualise concepts during their Classroom
+ Sessions but also help in their routine practice session to clear their doubts lively at home
+through cross learning on TELEGRAM AppGroup.We make children to post their doubts, let
+it be solved by many thoughts shared by other fellow friends/colleagues under the guidance
+and mentor ship of the friendly teacher.This helps in growth of thought process of a child
+ and create spirit of competitiveness.
+					</p> 
+        		</div>
+        	</div>
+        </section> -->
+        <!--================End About Area =================-->
+        
+        <!-- ================Team Area ================= -->
+         <!-- <section class="team_area p_120 ">
+        	<div class="container">
+        		<div class="main_title">
+        			<h2>Meet Our Faculty</h2>
+        			<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s exciting to think about setting up your own viewing station.</p>
+        		</div>
+        		<div class="row team_inner">
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="team_item">
+        					<div class="team_img">
+        						<img class="rounded-circle" src="img/team/team-1.jpg" alt="">
+        						<div class="hover">
+        							<a href="#"><i class="fa fa-facebook"></i></a>
+        							<a href="#"><i class="fa fa-twitter"></i></a>
+        							<a href="#"><i class="fa fa-linkedin"></i></a>
+        						</div>
+        					</div>
+        					<div class="team_name">
+        						<h4>Ethel Davis</h4>
+        						<p>Managing Director (Sales)</p>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="team_item">
+        					<div class="team_img">
+        						<img class="rounded-circle" src="img/team/team-2.jpg" alt="">
+        						<div class="hover">
+        							<a href="#"><i class="fa fa-facebook"></i></a>
+        							<a href="#"><i class="fa fa-twitter"></i></a>
+        							<a href="#"><i class="fa fa-linkedin"></i></a>
+        						</div>
+        					</div>
+        					<div class="team_name">
+        						<h4>Ethel Davis</h4>
+        						<p>Managing Director (Sales)</p>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="team_item">
+        					<div class="team_img">
+        						<img class="rounded-circle" src="img/team/team-3.jpg" alt="">
+        						<div class="hover">
+        							<a href="#"><i class="fa fa-facebook"></i></a>
+        							<a href="#"><i class="fa fa-twitter"></i></a>
+        							<a href="#"><i class="fa fa-linkedin"></i></a>
+        						</div>
+        					</div>
+        					<div class="team_name">
+        						<h4>Ethel Davis</h4>
+        						<p>Managing Director (Sales)</p>
+        					</div>
+        				</div>
+        			</div>
+        			<div class="col-lg-3 col-sm-6">
+        				<div class="team_item">
+        					<div class="team_img">
+        						<img class="rounded-circle" src="img/team/team-4.jpg" alt="">
+        						<div class="hover">
+        							<a href="#"><i class="fa fa-facebook"></i></a>
+        							<a href="#"><i class="fa fa-twitter"></i></a>
+        							<a href="#"><i class="fa fa-linkedin"></i></a>
+        						</div>
+        					</div>
+        					<div class="team_name">
+        						<h4>Ethel Davis</h4>
+        						<p>Managing Director (Sales)</p>
+        					</div>
+        				</div>
+        			</div>
+        		</div>
+        	</div>
+        </section> -->
+        <!--================End Team Area =================-->
+        
+        <!--================Testimonials Area =================-->
+        <section class="testimonials_area p_120">
+        	<div class="container">
+        		<div class="testi_slider owl-carousel">
+        			<div class="item">
+        				<div class="testi_item" style='border-color: red;border-width: thick;'>
+							<img src="img/mag.png" alt="">
+							   <h1 style='color:black;margin-top:12px;margin-bottom:1.5rem'>Vision & Mission</h1>
+							   <p style='font-family: "Open Sans", sans-serif;text-align: justify;font-size: 18px;
+    font-weight: 300;line-height: 26px;
+    color: #333;'>
+				<b style='font-weight:bold'><u style='color:black;'>Dream of Kindness-Free for Differently Abled Kids</u></b>:
+One of the wonderful initiatives of kindness started by our institution, Constant Classes, is
+helping the “Differently Abled” candidates, taking a first step towards success in achieving
+and visualising their dreams at NO COST.We at Constant Classes welcome all differently
+abled children having dreams in eyes and want to learn in the environment of equality and
+pride under the guidance of a friendly mentor cum guide cum friend. </p>
+<p style='font-family: "Open Sans", sans-serif;text-align: justify;font-size: 18px;
+    font-weight: 300;line-height: 26px;
+    color: #333;margin-bottom:15px'>
+<b style='font-weight:bold'><u style='color:black;'>Intuitive Learning Sessions:</u></b>
+The sessions at Constant Classes will be taken by Ms. Deepika Goyal (Former Assistant 
+Professor of Dronacharya College of Engineering and Former Mathematics Faculty for IIT
+JEE in Bansal Classes).We strive towards making children learn and visualise concepts of
+maths by mentoring and guiding them and making them so passionate to remove the fear of
+mathematics and feel mathematics as their fun-friend.</p>
+<p style='font-family: "Open Sans", sans-serif;text-align: justify;font-size: 18px;
+    font-weight: 300;line-height: 26px;
+    color: #333;'>A good learning is done when done with friends and the tutor at Constant Classes is not just
+a mentor or a guide, she is Band of Friendship and let children love mathematics. The only
+way to achieve a success in a great work is to love what you do.Intuitive Sessions at
+Constant Classes drive children towards visualisation of concepts of mathematics with ease
+and achieve great success. </p>
+<p style='font-family: "Open Sans", sans-serif;text-align: justify;font-size: 18px;
+    font-weight: 300;line-height: 26px;
+	color: #333;'>
+	<b style='font-weight:bold'><u style='color:black;'>Cross learning- Doubt Sessions anytime-anywhere:</u></b>
+We at Constant Classes not only help a child to visualise concepts during their Classroom
+ Sessions but also help in their routine practice session to clear their doubts lively at home
+through cross learning on TELEGRAM AppGroup.We make children to post their doubts, let
+it be solved by many thoughts shared by other fellow friends/colleagues under the guidance
+and mentor ship of the friendly teacher.This helps in growth of thought process of a child
+ and create spirit of competitiveness.
+					</p>
+       						<!-- <ul class="list">
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       						</ul> -->
+       						<!-- <p>Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.</p> -->
+        				</div>
+        			</div>
+        			<!-- <div class="item">
+        				<div class="testi_item">
+							<img src="img/testimonials/testi-3.png" alt="">
+       						<h4>Fannie Rowe</h4>
+       						<ul class="list">
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       						</ul>
+       						<p>Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.</p>
+        				</div>
+        			</div>
+        			<div class="item">
+        				<div class="testi_item">
+							<img src="img/testimonials/testi-3.png" alt="">
+       						<h4>Fannie Rowe</h4>
+       						<ul class="list">
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       							<li><a href="#"><i class="fa fa-star"></i></a></li>
+       						</ul>
+       						<p>Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.</p>
+        				</div>
+        			</div>
+        		</div> -->
+        	</div>
+        </section> 
+        <!--================End Testimonials Area =================-->
+        
+        <!--================Impress Area =================-->
+        <!-- <section class="impress_area p_120">
+        	<div class="container">
+        		<div class="impress_inner text-center">
+					<h2>Become an instructor</h2>
+					<p>There is a moment in the life of any aspiring astronomer that it is time to buy that first telescope. It’s exciting to think about setting up your own viewing station whether that is on the deck</p>
+					<a class="main_btn2" href="#">Apply for the post</a>
+        		</div>
+        	</div>
+        </section> -->
+</body>
+        <!--================End Impress Area =================-->
+        
+        <!--================ start footer Area  =================-->	
+<?php include 'footer.php';?>
+</html>
